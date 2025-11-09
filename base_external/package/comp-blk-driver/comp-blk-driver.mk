96d282e9ca07cceb3678346c56fb647fd9ca4208
Ubuntu_AESD <ubuntuaesd@UbuntuAESD.myguest.virtualbox.org>
2025-11-09 14:07:00 -0700

    build successful

---
+++ base_external/package/comp-blk-driver/comp-blk-driver.mk
@@ -0,0 +1,20 @@
+
+##############################################################
+#
+# comp-blk-driver
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+COMP_BLK_DRIVER_VERSION = 4c909d3c74d0df7a5748fa7704e1b9a3e10acd2e
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+COMP_BLK_DRIVER_SITE = git@github.com:cu-ecen-aeld/final-project-vaishnavijadhav19.git
+COMP_BLK_DRIVER_SITE_METHOD = git
+COMP_BLK_DRIVER_MODULE_SUBDIRS = comp-blk-driver
+
+
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
